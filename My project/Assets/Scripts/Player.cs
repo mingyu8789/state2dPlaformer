dbce3602de23189c1059ac07ba49cfc96a418523
박민규 <41883207+mingyu8789@users.noreply.github.com>
2025-04-14 17:55:27 +0900

    aa

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem.XInput;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     public float movespeed;
     public float jumppose;
+
+    public float flipdir = 1;
+    public bool flipbool = true;
+
+    public Transform Ground;
+    public float GroundLine;
+    public LayerMask WhatIsGround;
 
     public Rigidbody2D rb {  get; private set; }
     public Animator ani { get; private set; }
@@ -34,8 +43,29 @@ public class Player : MonoBehaviour
         stateMashine.state.Update();
     }
 
+    public void Flip()
+    {
+        flipdir = flipdir * -1;
+        flipbool = !flipbool;
+        transform.Rotate(0, 180, 0);
+    }
+
+    public void FlipController(float _x)
+    {
+        if (_x > 0 && !flipbool)
+            Flip();
+        else if (_x < 0 && flipbool)
+            Flip();
+    }
+
     public void PlayerlineVelocity(float xlineVelocity, float ylineVelocity)
     {
         rb.linearVelocity = new Vector2 (xlineVelocity, ylineVelocity);
+        FlipController(xlineVelocity);
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(Ground.position, new Vector3(Ground.position.x * GroundLine, Ground.position.y));
     }
 }
---
+++ My project/Assets/Scripts/PlayerGroundCheck.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerGroundCheck : PlayerState
+{
+    public PlayerGroundCheck(Player _player, PlayerStateMashine _statemashine, string _aniboolname) : base(_player, _statemashine, _aniboolname)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
